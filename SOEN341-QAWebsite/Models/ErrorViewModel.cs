ceee2e06b3e2b14d5df3f19b80f292626379f35b
Ofer Nitka-Nakash <ofer.nitka@gmail.com>
2018-01-27 21:32:11 -0500

    initial scaffold and setup for project.

---
+++ SOEN341-QAWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SOEN341_QAWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SOEN341-QAWebsite/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOEN341_QAWebsite.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
