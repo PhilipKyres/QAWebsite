280a39d272875f22c2f35c8beef88fa53ef01a08
Ofer Nitka-Nakash <ofer.nitka@gmail.com>
2018-03-18 19:57:23 -0400

    #106 moved sensitive information for email and initial admin account into external json files

--- QAWebsite/Data/DBInitializer.cs
+++ QAWebsite/Data/DBInitializer.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using QAWebsite.Models;
 using QAWebsite.Models.Enums;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +12,11 @@ namespace QAWebsite.Data
     {
         public static async Task SeedAsync(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
         {
+            var config = new ConfigurationBuilder()
+	        .SetBasePath(Directory.GetCurrentDirectory())
+	        .AddJsonFile("seedingData.json")
+	        .Build();
+            
             var userRole = roleManager.Roles.FirstOrDefault(role => role.NormalizedName == Roles.User);
             if (userRole == null)
             {
@@ -23,18 +30,18 @@ namespace QAWebsite.Data
                 adminRole = new ApplicationRole(Roles.Administrator);
                 await roleManager.CreateAsync(adminRole);
             }
-
-            var admin = userManager.Users.FirstOrDefault(user => user.NormalizedUserName == "Admin@QAWebsite.ca".Normalize());
+            var adminSection = config.GetSection("AdministratorAccount");
+            var admin = userManager.Users.FirstOrDefault(user => user.NormalizedUserName == adminSection["AdministratorUsername"].Normalize());
             if (admin == null)
             {
                 var user = new ApplicationUser
                 {
-                    UserName = "Admin@QAWebsite.ca",
-                    Email = "Admin@QAWebsite.ca",
+                    UserName = adminSection["AdministratorUsername"],
+                    Email = adminSection["AdministratorEmail"],
                     EmailConfirmed = true
                 };
 
-                await userManager.CreateAsync(user, "!Qaz2wsx");
+                await userManager.CreateAsync(user, adminSection["AdministratorPassword"]);
                 await userManager.AddToRoleAsync(user, Roles.Administrator);
             }
 
